560a4eae9db7e2a08f3be6b786db07ea85e0a9e4
Moshik Hershcovitch <moshik1@gmail.com>
2024-08-13 19:00:39 +0300

    v0.2.4

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ split_dtype_extension = Extension(
 
 setup(
     name='zipnn',
-    version='0.2.3',
+    version='0.2.4',
     author='Moshik Hershcovitch',
     author_email='moshik1@gmail.com',
     description='A lossless and near-lossless compression method optimized for numbers/tensors in the Foundation Models environment',
--- zipnn/zipnn.py
+++ zipnn/zipnn.py
@@ -185,7 +185,7 @@ class ZipNN:
 
         self._version_major = 0
         self._version_minor = 2
-        self._version_tiny = 3
+        self._version_tiny = 4
         self._import_dependencies(zstd_level)
 
         HEADER_LEN=20
