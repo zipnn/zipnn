d23b9565992ec9c7f162b9bfcdb9f30a3553be36
Guy Girmonsky <guygir@gmail.com>
2024-08-14 15:05:51 +0300

    Integrated compress to run_compression and decompress to run_decompression

--- scripts/run_compression_zipnn.py
+++ scripts/run_compression_zipnn.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from zipnn import ZipNN
 import sys
 import argparse
 
@@ -11,9 +12,57 @@ def check_and_install_zipnn():
         subprocess.check_call([sys.executable, "-m", "pip", "install", "zipnn"])
         import zipnn
 
+def parse_streaming_chunk_size(streaming_chunk_size):
+    if str(streaming_chunk_size).isdigit():
+        final = int(streaming_chunk_size)  # Convert the digit string to an integer
+    else:
+        # Extract the numeric part of the string
+        size_value = int(streaming_chunk_size[:-2])
+        size_unit = streaming_chunk_size[-2].lower()
+
+        if size_unit == 'k':
+            final = 1024 * size_value
+        elif size_unit == 'm':
+            final = 1024 * 1024 * size_value
+        elif size_unit == 'g':
+            final = 1024 * 1024 * 1024 * size_value
+        else:
+            raise ValueError(f"Invalid size unit: {size_unit}. Use 'k', 'm', or 'g'.")
+        
+    return final
+
+def compress_file(input_file,dtype="",streaming_chunk_size=1048576):
+    # Define the output file name by adding the '.zpn' suffix
+    output_file = input_file + '.zpn'
+    
+    # Init ZipNN
+    #streaming_chunk_size=1048576 #1MB
+    if dtype:
+        zipnn = ZipNN(bytearray_dtype='float32')
+    else:
+        zipnn = ZipNN()
+
+    
+    # Compress
+    file_size_before=0
+    file_size_after=0
+    with open(input_file, 'rb') as infile, open(output_file, 'wb') as outfile:
+        while chunk := infile.read(streaming_chunk_size):
+            file_size_before+=len(chunk)
+            compressed_chunk = zipnn.compress(chunk)
+            if compressed_chunk:
+                file_size_after+=len(compressed_chunk)
+                outfile.write(compressed_chunk)
+        print(f"Compressed {input_file} to {output_file}")
+        print ("Original size: "+str(file_size_before)+", size after compression: "+str(file_size_after)+", Remaining size is "+str(file_size_after/file_size_before*100)+"% of original")
+
+
 def compress_files_with_suffix(suffix,dtype="",streaming_chunk_size=1048576,path="."):
     import zipnn
     from zipnn_compress import compress_file
+
+    # Handle streaming chunk size
+    streaming_chunk_size=parse_streaming_chunk_size(streaming_chunk_size)
 
     # List all files in the current directory
     files_found = False
@@ -35,8 +84,8 @@ if __name__ == "__main__":
         print("Example: python compress_files.py 'safetensors'")
         sys.exit(1)
 
-    parser = argparse.ArgumentParser(description="Enter a suffix to compress, (optional) dtype, (optional) streaming chunk size.")
-    parser.add_argument('suffix', type=str, help='The file to compress')
+    parser = argparse.ArgumentParser(description="Enter a suffix to compress, (optional) dtype, (optional) streaming chunk size, (optional) path to files.")
+    parser.add_argument('suffix', type=str, help='Specify the file suffix to compress all files with that suffix. If a single file name is provided, only that file will be compressed.')
     parser.add_argument('--float32', action='store_true', help='A flag that triggers float32 compression')
     parser.add_argument('--streaming_chunk_size', type=str, help='An optional streaming chunk size. The format is int (for size in Bytes) or int+KB/MB/GB. Default is 1MB')
     parser.add_argument('--path', type=str, help='Path to files to compress')
--- scripts/run_decompression_zipnn.py
+++ scripts/run_decompression_zipnn.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import sys
 import argparse
+from zipnn import ZipNN
 
 def check_and_install_zipnn():
     try:
@@ -11,9 +12,16 @@ def check_and_install_zipnn():
         subprocess.check_call([sys.executable, "-m", "pip", "install", "zipnn"])
         import zipnn
 
-def decompress_zpn_files(dtype="",path="."):
+def decompress_zpn_files(dtype="",path=".",input_file=None):
     import zipnn
     from zipnn_decompress import decompress_file
+
+    if input_file:
+        if (path!="."):
+            input_file=path+"/"+input_file
+        print(f"Decompressing {input_file}...")
+        decompress_file(input_file,dtype=dtype)
+        exit(0)
     
     # List all files in the current directory
     for file_name in os.listdir(path):
@@ -24,18 +32,50 @@ def decompress_zpn_files(dtype="",path="."):
                 file_name=path+"/"+file_name
             decompress_file(file_name,dtype=dtype)
 
+def decompress_file(input_file,dtype=""):
+    # Verify the file suffix
+    if not input_file.endswith('.zpn'):
+        raise ValueError("Input file does not have the '.zpn' suffix")
+
+    # Define the output file name by removing the '.zpn' suffix
+    output_file = input_file[:-4]
+
+    # Init ZipNN
+    if dtype:
+        zipnn = ZipNN(bytearray_dtype='float32')
+    else:
+        zipnn = ZipNN()
+    
+    # Decompress
+    with open(input_file, 'rb') as infile, open(output_file, 'wb') as outfile:
+        d_data=b''
+        while header:= infile.read(20):
+            mv_header=memoryview(header)
+            mid_chunk_len=int.from_bytes(mv_header[16:20], byteorder="little")-20
+            chunk_data = infile.read(mid_chunk_len)
+            decompressed_chunk = zipnn.decompress(header + chunk_data)
+            if decompressed_chunk:
+                d_data+=decompressed_chunk
+                outfile.write(d_data)
+                d_data=b''
+        print(f"Decompressed {input_file} to {output_file}")
+
+
 if __name__ == "__main__":
     check_and_install_zipnn()
 
     parser = argparse.ArgumentParser(description="Compresses all .zpn files. (optional) dtype.")
-    parser.add_argument('--float32', action='store_true', help='A flag that triggers float32 compression')
-    parser.add_argument('--path', type=str, help='Path to files to decompress')
+    parser.add_argument('--float32', action='store_true', help='A flag that triggers float32 compression.')
+    parser.add_argument('--path', type=str, help='Path to folder of files to decompress.')
+    parser.add_argument('--input_file', type=str, help='Name of file if only a single file needs decompression.')
     args = parser.parse_args()
     optional_kwargs = {}
     if args.float32:
         optional_kwargs['dtype'] = 32
     if args.path is not None:
         optional_kwargs['path'] = args.path
+    if args.input_file is not None:
+        optional_kwargs['input_file'] = args.input_file
     
     decompress_zpn_files(**optional_kwargs)
 
