1b2984a92ff1699e44657592ea392d3273835dab
Guy Girmonsky <guygir@gmail.com>
2024-08-25 11:21:26 +0300

    Streaming isn't supporting tensor yet

--- zipnn/zipnn.py
+++ zipnn/zipnn.py
@@ -433,9 +433,8 @@ class ZipNN:
         in the format chosen in the ZipNN class instance configuration.
         """
 
-        mv_data=memoryview(data)
-        rnd=0
-        if(self.is_streaming):
+        if(self.is_streaming and input_format!="torch"):
+            mv_data=memoryview(data)
             CHUNK_SIZE = self.streaming_chunk_kb
             
             # Compression into bytearray
@@ -878,7 +877,7 @@ class ZipNN:
 
         mv_data=memoryview(data)
         comp_chunk_size=mv_data[13] #0 if no streaming > 127
-        if(comp_chunk_size>127):
+        if(comp_chunk_size>127 and input_format!="torch"):
             decompressed_buffer = bytearray()
             offset = 0
             compressed_length = len(data)
