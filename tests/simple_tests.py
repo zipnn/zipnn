5a0832f2ad80ebc8da101ce804d8116a3eeca9ec
Guy Girmonsky <guygir@gmail.com>
2024-09-15 14:04:14 +0300

    Added delta comp/decomp and tests

--- tests/simple_tests.py
+++ tests/simple_tests.py
@@ -67,3 +67,74 @@ def test_byte_torch_streaming():
         compressed_data = zpn_streaming.compress(original_bytes)
         decompressed_data = zpn_streaming.decompress(compressed_data)
         print("Are the original and decompressed byte strings the same [STREAMING BYTES]? ",copy_bytes == decompressed_data)
+
+    # Delta(byte)
+    zpn_delta = ZipNN(delta_compressed_type="byte")
+    for size in [int(1024*10)]:
+        a=create_random_bytes(int(size))
+        b=create_random_bytes(int(size))
+        c=create_random_bytes(int(size))
+        original_bytes=a+b
+        second_data=a+c
+        copy_bytes=bytearray(original_bytes)
+        compressed_data = zpn_delta.compress(original_bytes,delta_second_data=second_data)
+        decompressed_data = zpn_delta.decompress(compressed_data,delta_second_data=second_data)
+        print("Are the original and decompressed byte strings the same [DELTA BUFFER]? ", copy_bytes == decompressed_data)
+        if not copy_bytes == decompressed_data:
+            raise ValueError("Error - original file and decompressed file are NOT equal.")
+
+    # Streaming delta(byte)
+    for size in [int(1024*8)]:
+        zpn_streaming_delta = ZipNN(delta_compressed_type="byte",is_streaming=True)
+        a=create_random_bytes(int(size))
+        b=create_random_bytes(int(size))
+        c=create_random_bytes(int(size))
+        original_bytes=a+b
+        second_data=a+c
+        copy_bytes=bytearray(original_bytes)
+        compressed_data = zpn_streaming_delta.compress(original_bytes,delta_second_data=second_data)
+        decompressed_data = zpn_streaming_delta.decompress(compressed_data,delta_second_data=second_data)
+        print("Are the original and decompressed byte strings the same [STREAMING DELTA BUFFER]? ", copy_bytes == decompressed_data)
+        if not copy_bytes == decompressed_data:
+            raise ValueError("Error - original file and decompressed file are NOT equal.")
+
+    # Delta from file
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, 'temp_file.bin')
+    for size in [int(1024*8)]:
+        zpn_streaming_delta = ZipNN(delta_compressed_type="file")
+        a=create_random_bytes(int(size))
+        b=create_random_bytes(int(size))
+        c=create_random_bytes(int(size))
+        original_bytes=a+b
+        second_data=a+c
+        # Write second_data to file
+        with open(file_path, 'wb') as f:
+            f.write(second_data)
+        copy_bytes=bytearray(original_bytes)
+        compressed_data = zpn_streaming_delta.compress(original_bytes,delta_second_data=file_path)
+        decompressed_data = zpn_streaming_delta.decompress(compressed_data,delta_second_data=file_path)
+        print("Are the original and decompressed byte strings the same [DELTA FILE]? ", copy_bytes == decompressed_data)
+        if not copy_bytes == decompressed_data:
+            raise ValueError("Error - original file and decompressed file are NOT equal.")
+
+    # Streaming delta from file
+    for size in [int(1024*8)]:
+        zpn_streaming_delta = ZipNN(delta_compressed_type="file",is_streaming=True)
+        a=create_random_bytes(int(size))
+        b=create_random_bytes(int(size))
+        c=create_random_bytes(int(size))
+        original_bytes=a+b
+        second_data=a+c
+        # Write second_data to file
+        with open(file_path, 'wb') as f:
+            f.write(second_data)
+        copy_bytes=bytearray(original_bytes)
+        compressed_data = zpn_streaming_delta.compress(original_bytes,delta_second_data=file_path)
+        decompressed_data = zpn_streaming_delta.decompress(compressed_data,delta_second_data=file_path)
+        print("Are the original and decompressed byte strings the same [DELTA FILE STREAMING]? ", copy_bytes == decompressed_data)
+        if not copy_bytes == decompressed_data:
+            raise ValueError("Error - original file and decompressed file are NOT equal.") 
+
+    if os.path.exists(file_path):
+        os.remove(file_path)
