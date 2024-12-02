b035c559a3683e3f8edf4131777bbaac86528540
Tal Regev <tal.regev@gmail.com>
2024-12-02 20:26:37 +0200

    array with no size define on declaration of function

--- csrc/split_dtype.c
+++ csrc/split_dtype.c
@@ -12,10 +12,10 @@
 ////  Helper Functions ///////
 uint8_t *prepare_split_results(size_t header_len, size_t numBuf,
                                 size_t numChunks, uint8_t *header,
-                                uint8_t *compressedData[numBuf][numChunks],
-                                uint32_t compChunksSize[numBuf][numChunks],
-                                const uint8_t compChunksType[numBuf][numChunks],
-                                const size_t cumulativeChunksSize[numBuf][numChunks],
+                                uint8_t *compressedData[][],
+                                uint32_t compChunksSize[][],
+                                const uint8_t compChunksType[][],
+                                const size_t cumulativeChunksSize[][],
                                 const size_t *totalCompressedSize,
                                 size_t *resBufSize) {
   *resBufSize = header_len;
